0bd32150af5c1c8ccec60c0688c6c7fc4aae4033
anhaabaete <anhaabaete@gmail.com>
2019-05-06 11:31:10 -0300

    Arquivo do arduino

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
-function verCodigo() {
+
+function abrirNoArduino() {
     var code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
-    alert(code);
+
+    var filename = getFilename();
+    
+    code = btoa(code);
+
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = 'data:text/ino;base64,' + code;
+    document.body.appendChild(link);
+    link.click();
+
+}
+
+function getFilename() {
+    var dt = new Date();
+    toString()
+    
+    return 'oya' + dt.getTime().toString().substr(-8) + '.ino';
 }
 
